bea26a3c5575beb392eb8a91b4d8bf11ec780115
BeatricePapi-STUDENT <G00461762@atu.ie>
2025-10-06 08:51:36 +0100

    Project setup

---
+++ src/main/java/ie/atu/week2/Student.java
@@ -0,0 +1,7 @@
+package ie.atu.week2;
+
+public class Student {
+    private String name;
+    private String email;
+    private String course;
+}
---
+++ src/main/java/ie/atu/week2/StudentApp.java
@@ -0,0 +1,6 @@
+package ie.atu.week2;
+
+import java.util.Scanner;
+
+public class StudentApp {
+}
