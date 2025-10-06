8e33c849764dc0c5e3affbc0aa32e1224afaf5af
BeatricePapi-STUDENT <G00461762@atu.ie>
2025-10-06 09:44:16 +0100

    feature/arraylist

--- src/main/java/ie/atu/week2/StudentApp.java
+++ src/main/java/ie/atu/week2/StudentApp.java
@@ -1,6 +1,11 @@
 package ie.atu.week2;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StudentApp {
+    public static void main(String[] args) {
+        // Holds all Student objects
+        ArrayList<Student> students = new ArrayList<>();
+    }
 }
